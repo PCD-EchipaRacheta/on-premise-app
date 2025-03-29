084e95fd13b3304b1a22b26facc08e9cd84344cb
Andrei Aldea <aldea.andrei.977@gmail.com>
2025-03-29 20:09:40 +0200

    succesfull use of bigquery

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,25 @@ from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 from datetime import datetime
 
+from google.cloud import bigquery
+import os
+
+# Set the environment variable GOOGLE_APPLICATION_CREDENTIALS to the path of your JSON key file
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "account.json"
+
+# Construct a BigQuery client object.
+client = bigquery.Client()
+
+dataset_id = "weatherapp-455117.weather_data"
+tables = list(client.list_tables(dataset_id))
+table_ids = list(map(lambda x: x.table_id, tables))
+
 app = FastAPI()
 
 # Allow requests from your React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Vite default
+    allow_origins=["*"],  # Vite default
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -38,3 +51,7 @@ weather_data = [
 def get_weather(city: str = Query(..., description="City name")):
     results = [entry for entry in weather_data if entry["city"].lower() == city.lower()]
     return results
+
+@app.get("/tables")
+def get_tables():
+    return table_ids
